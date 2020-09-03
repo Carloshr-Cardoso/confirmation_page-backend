3cbcacf586c4df8e55c3f5e5bdccda8b60485cfd
CarloshrCardoso <carloshr.cardoso@outlook.com>
2020-09-03 16:59:58 -0300

    Config Sequelize and Mysql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const authController = require('./Controllers/auth')
+const db = require('./models')
+
 const app = express();
 
 
@@ -9,6 +11,9 @@ app.get('/', (req, res)=>{
     return res.json("Baby Shower API Running...")
 })
 
-app.listen(3001, ()=>{ 
-    console.log("listen on Port 3001")
-})
+
+db.sequelize.sync().then(()=>{
+    app.listen(3001, ()=>{ 
+        console.log("listen on Port 3001")
+    });
+});
