acb7f8b51ff2441ca17c2e3987961016fdb11f01
CarloshrCardoso <carloshr.cardoso@outlook.com>
2020-09-08 15:36:44 -0300

    Excluindo Rotas de SignIn e SignUp da verificação de Token

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,19 @@
 const { verifyJwt, verifyRefreshJwt } = require('../helpers/jwt');
 
 const checkJwt = (req, res, next) =>{
+    // /auth/sign-in
+    // /auth/sign-up
+
+    const { url:path } = req
+
+    const excludedPaths = ['/auth/sign-in', '/auth/sign-up']
+    const isExcluded = !!excludedPaths.find(p => p.startsWith(path));
+    if (isExcluded){
+        return next();
+    }
+
+    console.log('is Excluded: ', isExcluded)
+
     let token = req.headers['authorization'];
     token = token ? token.slice(7, token.length) : null;
     if (!token){
